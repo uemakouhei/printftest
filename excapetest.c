957c880b44b4b9ae11376fc27d010e8154098095
上間公平 <uemakouhei@uemakouheinoMacBook-Air.local>
2021-12-16 16:12:20 +0900

    1

--- excapetest.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	int i;
-
-	i = printf("%100%stsr");
-	printf("char count : %d ",i);
-}
--- freetestspace.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(void)
-{
-	write(1," ",5);
-	write(1,"a",1);
-}
